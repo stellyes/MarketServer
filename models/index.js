9b147f85b4caaeda99a91371e920810b04e6f014
Ryan England <me@rengland.org>
2023-11-03 15:28:10 -0700

    Fixed seeding error in seeds/index.js

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "product_id",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
